03fefbb21c005290d70db6fb539075579931d728
Learner Learner <mehemmedmehdi2003@gmail.com>
2024-12-03 12:00:11 +0400

    Basic Template for All Functionalities

--- gui.py
+++ gui.py
@@ -1,28 +1,63 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QMessageBox, QVBoxLayout, QWidget, QTextEdit, QListWidget, QListWidgetItem, QAbstractItemView
 import sys
 
-class MainWindow(QMainWindow):
-    instruction_selected = False
+
+class NumberOfQuestions:
+    choices = [5, 10, 15, 20]
+
+class DifficultyLevel:
+    choices = ["easy", "medium", "hard"]
+
+class Category:
+    choices = ["numerical reasoning", "verbal reasoning", "situational judgment"]
+
+class Format:
+    choices = ["multiple-choice", "fill-in-the-blank", "true/false"]
+
+class Customization:
+    features = [NumberOfQuestions, DifficultyLevel, Category, Format]
+
+class MainWindow(QMainWindow, Customization):
+    number_of_questions_selected = False
+    difficulty_level_selected = False
+    category_selected = False
+    format_selected = False
 
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("App")
-        self.setGeometry(100, 100, 200, 300)
+        self.setGeometry(700, 200, 400, 600)
 
-        label = QLabel("Choose the number of questions")
-        selection = QListWidget()
-        selection.setSelectionMode(QAbstractItemView.NoSelection)
-        selection.setGeometry(100, 100, 200, 300)
-        self.__upload_items(selection)
-        selection.currentRowChanged.connect(lambda : self.selection_triggered(selection))
+        questions_label = QLabel("Choose the number of questions")
+        difficulty_label = QLabel("Choose a difficulty level")
+        category_label = QLabel("Choose a category")
+        format_label = QLabel("Choose a format")
+
+        number_of_questions = self.__create_selection(0)
+        number_of_questions.currentRowChanged.connect(lambda : self.number_of_questions_triggered(number_of_questions))
         
+        difficulty_level = self.__create_selection(1)
+        difficulty_level.currentRowChanged.connect(lambda : self.difficulty_level_triggered(difficulty_level))
+
+        category = self.__create_selection(2)
+        category.currentRowChanged.connect(lambda : self.category_triggered(category))
+        
+        format = self.__create_selection(3)
+        format.currentRowChanged.connect(lambda x: self.format_triggered(format))
+
         button = QPushButton("Button")
-        button.clicked.connect(lambda : self.button_triggered(selection))
+        button.clicked.connect(lambda : self.button_triggered(number_of_questions, difficulty_level, category, format))
         
         layout = QVBoxLayout()
-        layout.addWidget(label)
-        layout.addWidget(selection)
+        layout.addWidget(questions_label)
+        layout.addWidget(number_of_questions)
+        layout.addWidget(difficulty_label)
+        layout.addWidget(difficulty_level)
+        layout.addWidget(category_label)
+        layout.addWidget(category)
+        layout.addWidget(format_label)
+        layout.addWidget(format)
         layout.addWidget(button)
 
         widget = QWidget()
@@ -30,32 +65,92 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(widget)
     
-    def button_triggered(self, selection):
+    def button_triggered(self, number_of_questions, difficulty_level, category, format):
+        self.run = True
+        message = ""
+        if not self.number_of_questions_selected:
+            sentence = "Please, choose the number of questions\n"
+            message = message + sentence
+            self.run = False
 
-        if not self.instruction_selected:
-            msg = "Please, choose the number of questions"        
-        else:
-            n_questions = self.items[selection.currentRow()].text()
-            msg = "Model started processing on {} questions".format(n_questions)
+        if not self.difficulty_level_selected:
+            sentence = "Please, choose a difficulty level\n"
+            message = message + sentence
+            self.run = False
+        
+        if not self.category_selected:
+            sentence = "Please, choose a category\n"
+            message = message + sentence
+            self.run = False
+
+        if not self.format_selected:
+            sentence = "Please, choose a format\n"
+            message = message + sentence
+            self.run = False
+
+        if self.run:
+            choices = self.features[0].choices
+            n_questions_value = choices[number_of_questions.currentRow()]
+
+            choices = self.features[1].choices
+            difficulty_value = choices[difficulty_level.currentRow()]
+
+            choices = self.features[2].choices
+            category_value = choices[category.currentRow()]
+
+            choices = self.features[3].choices
+            format_value = choices[format.currentRow()]
+
+            message = "Model started processing on {} questions, {} difficulty level, {} category, and {} format".format(
+                n_questions_value, 
+                difficulty_value, 
+                category_value,
+                format_value)
+            
+            number_of_questions.setCurrentRow(-1)
+            difficulty_level.setCurrentRow(-1)
+            category.setCurrentRow(-1)
+            format.setCurrentRow(-1)
+
+        message_box = QMessageBox()
+        message_box.setText(message)
+        message_box.setGeometry(800, 500, 500, 500)
+        message_box.exec_()
+
+        if self.run:
             self.run_model()
-            self.instruction_selected = False
-            selection.setCurrentRow(-1)
+            self.number_of_questions_selected = False
+            self.difficulty_level_selected = False
+            self.category_selected = False
+            self.format_selected = False
 
-        message = QMessageBox()
-        message.setText(msg)
-        message.setGeometry(700, 700, 500, 500)
-        message.exec_()
+    def __upload_items(self, selection, idx):
+        for choice in self.features[idx].choices:
+            item = QListWidgetItem(str(choice))
+            selection.addItem(item)
 
-    def __upload_items(self, selection):
-        item_0 = QListWidgetItem("10")
-        item_1 = QListWidgetItem("20")
-        selection.addItem(item_0)
-        selection.addItem(item_1)
-        self.items = [item_0, item_1]
+    def __create_selection(self, idx):
+        selection = QListWidget()
+        selection.setSelectionMode(QAbstractItemView.NoSelection)
+        selection.setGeometry(700, 900, 100, 200)
+        self.__upload_items(selection, idx)
+        return selection
     
-    def selection_triggered(self, selection):
+    def number_of_questions_triggered(self, selection):
         if selection.currentRow() != -1:
-            self.instruction_selected = True
+            self.number_of_questions_selected = True
+
+    def difficulty_level_triggered(self, selection):
+        if selection.currentRow() != -1:
+            self.difficulty_level_selected = True
+
+    def category_triggered(self, selection):
+        if selection.currentRow() != -1:
+            self.category_selected = True
+    
+    def format_triggered(self, selection):
+        if selection.currentRow() != -1:
+            self.format_selected = True
 
     def run_model(self):
         print("There you go")
